90f68dd28f7c697921211decc1530d5740c26f2c
Alp <Kadiralpcil@gmail.com>
2025-08-05 16:31:55 +0300

    dark mode implemented

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body
+        className={`${inter.className} bg-background text-foreground min-h-screen`}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
